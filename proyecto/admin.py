539b20ca104d44609569b3d5ddfe0bff93adc2c7
juanMartinRodrigo <juanmanuelmartinrodrigo@gmail.com>
2020-07-07 11:18:20 -0300

    Se le agrego cosas al admin

--- proyecto/admin.py
+++ proyecto/admin.py
@@ -1,19 +1,42 @@
 from django.contrib import admin
 from proyecto.models import *
 
-class CategoriaInline(admin.TabularInline):
-    model = Categoria
+class ProductoInline(admin.TabularInline):
+    model = Producto
 
-class VentaInline(admin.TabularInline):
-    model = Venta
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'telefono', ]
+
+    search_fields = ['nombre', 'telefono',]
 
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'precio',]
-    inlines = [CategoriaInline , VentaInline, ]
+    fieldsets = (
+        ('Drescripcion',{
+            'fields': ('nombre',)
+        }),
+        ('Variables',{
+            'fields': ('stock', 'precio',)
+        }),
+        ('Detalles',{
+            'fields': ('categoria', 'proveedor', )
+        }),
+    )
 
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'telefono', 'web', 'direccion']
 
+    search_fields = ['nombre', 'telefono',]
+
+    inlines = [ProductoInline,]
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ['cliente', 'producto', 'cantidad', 'monto_final', 'fecha','des', ]
+
+    #exclude = ['montoFinal']
 
 # Register your models here.
-admin.site.register(Producto, ProductoAdmin );
-admin.site.register(Proveedor);
-admin.site.register(Venta);
+admin.site.register(Venta, VentaAdmin,)
+admin.site.register(Cliente, ClienteAdmin,)
+admin.site.register(Proveedor, ProveedorAdmin,)
+admin.site.register(Producto, ProductoAdmin,)
+admin.site.register(Categoria,)
+admin.site.register(Direccion,)
--- proyecto/models.py
+++ proyecto/models.py
@@ -5,48 +5,64 @@ class Direccion(models.Model):
     calle = models.CharField(max_length=50, default="")
     comuna = models.CharField(max_length=50, default="")
     ciudad = models.CharField(max_length=50, default="")
-    numero = models.IntegerField()
+    numero = models.IntegerField(null = True)
+    
     def __str__(self):
         return str(self.ciudad)
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50, default="")
     descripcion = models.CharField(max_length=50, default="")
+
     def __str__(self):
-        return str(self.nombre + " " + self.descripcion)
+        return str(self.nombre)
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=50, default="")
-    precio =  models.FloatField()
-    stock =  models.IntegerField()
+    precio =  models.FloatField(null = True)
+    stock =  models.IntegerField(null = True)
     categoria = models.ForeignKey('Categoria', default = None, on_delete=models.CASCADE)
     proveedor = models.ForeignKey('Proveedor', default = None, on_delete=models.CASCADE)
+    
     def __str__(self):
         return str(self.nombre)
 
 class Proveedor(models.Model):
-    telefono = models.IntegerField()
+    telefono = models.IntegerField(null = True)
     nombre = models.CharField(max_length=50, default="")
     web = models.CharField(max_length=50, default="")
-    rut = models.IntegerField()
-     direccion = models.ForeignKey('Direccion', default = None, on_delete=models.CASCADE)
+    rut = models.IntegerField(null = False, unique = True)
+    direccion = models.ForeignKey('Direccion', default = None, on_delete=models.CASCADE)
+    
     def __str__(self):
         return str(self.nombre)
 
 class Venta(models.Model):
     fecha = models.DateField(auto_now_add=True)
-    descuento = models.BooleanField()
-    cantidad = models.IntegerField()
+    descuento = models.BooleanField(null = True)
+    cantidad = models.IntegerField(null = True)
     cliente = models.ForeignKey('Cliente', default = None, on_delete=models.CASCADE)
     producto = models.ForeignKey('Producto', default = None, on_delete=models.CASCADE)
-    def monto_final():
-        self.monto = self.producto.precio * self.cantidad
-        return self.monto
+    montoFinal = models.FloatField()
+    
+    def des(self):
+        if self.descuento == True:
+            return True
+        else:
+            return False
+    
+    des.boolean = True
+    des.short_description = "Tiene descuento"
+
+    def monto_final(self):
+        self.montoFinal = self.producto.precio * self.cantidad
+        return self.montoFinal
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50, default="")
     direccion = models.ManyToManyField('Direccion')
-    telefono = models.IntegerField()
-    rut = models.IntegerField()
+    telefono = models.IntegerField(null = True)
+    rut = models.IntegerField(null = False, unique = True)
+    
     def __str__(self):
-        return str(self.nombre + " " + self.direccion)
+        return str(self.nombre)
